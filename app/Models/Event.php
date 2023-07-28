1a68b61167f0aa5a7c39d3a384e367919e2993bc
Sherxon <116145014+SherxonBoboyorov@users.noreply.github.com>
2023-07-28 18:33:14 +0500

    changes

--- app/Models/Event.php
+++ app/Models/Event.php
@@ -4,8 +4,78 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\File;
 
 class Event extends Model
 {
     use HasFactory;
+
+    protected $table = 'events';
+
+    protected $fillable = [
+        'image',
+        'title_ru',
+        'title_uz',
+        'title_en',
+        'slug_ru',
+        'slug_uz',
+        'slug_en',
+        'content_ru',
+        'content_uz',
+        'content_en',
+        'frame',
+        'meta_title_ru',
+        'meta_title_uz',
+        'meta_title_en',
+        'meta_description_ru',
+        'meta_description_uz',
+        'meta_description_en',
+    ];
+
+
+    public static function uploadImage($request): ?string
+    {
+        if ($request->hasFile('image')) {
+
+            self::checkDirectory();
+
+            $request->file('image')
+                ->move(
+                    public_path() . '/upload/event/' . date('d-m-Y'),
+                    $request->file('image')->getClientOriginalName()
+                );
+            return '/upload/event/' . date('d-m-Y') . '/' . $request->file('image')->getClientOriginalName();
+        }
+
+        return null;
+    }
+
+    public static function updateImage($request, $event): string
+    {
+        if ($request->hasFile('image')) {
+            if (File::exists(public_path() . $event->image)) {
+                File::delete(public_path() . $event->image);
+            }
+
+            self::checkDirectory();
+
+            $request->file('image')
+                ->move(
+                    public_path() . '/upload/event/' . date('d-m-Y'),
+                    $request->file('image')->getClientOriginalName()
+                );
+            return '/upload/event/' . date('d-m-Y') . '/' . $request->file('image')->getClientOriginalName();
+        }
+
+        return $event->image;
+    }
+
+    private static function checkDirectory(): bool
+    {
+        if (!File::exists(public_path() . '/upload/event/' . date('d-m-Y'))) {
+            File::makeDirectory(public_path() . '/upload/event/' . date('d-m-Y'), $mode = 0777, true, true);
+        }
+
+        return true;
+    }
 }
--- database/migrations/2023_07_28_131343_create_events_table.php
+++ database/migrations/2023_07_28_131343_create_events_table.php
@@ -15,6 +15,32 @@ return new class extends Migration
     {
         Schema::create('events', function (Blueprint $table) {
             $table->id();
+
+            $table->string('image');
+
+            $table->string('title_uz');
+            $table->string('title_en');
+            $table->string('title_ru');
+
+            $table->string('slug_uz');
+            $table->string('slug_en');
+            $table->string('slug_ru');
+
+            $table->text('content_uz');
+            $table->text('content_en');
+            $table->text('content_ru');
+
+            $table->text('frame');
+
+            $table->text('meta_title_ru')->nullable();
+            $table->text('meta_title_uz')->nullable();
+            $table->text('meta_title_en')->nullable();
+
+            $table->text('meta_description_ru')->nullable();
+            $table->text('meta_description_uz')->nullable();
+            $table->text('meta_description_en')->nullable();
+
+
             $table->timestamps();
         });
     }
