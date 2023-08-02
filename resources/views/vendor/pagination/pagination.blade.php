6ad33e3083b13adc8834a8fd05a7ef7c4f0053b9
Sherxon <116145014+SherxonBoboyorov@users.noreply.github.com>
2023-08-02 18:09:28 +0500

    changes

--- resources/views/vendor/pagination/pagination.blade.php
+++ resources/views/vendor/pagination/pagination.blade.php
@@ -1,35 +1,23 @@
 @if ($paginator->hasPages())
-   <nav>
-    {{-- <div class="blogIn__pagination"> --}}
-        <ul class="blogIn__pagination__page">
+        <div class="center">
+            <div class="pagination">
             @if (!$paginator->onFirstPage())
-                <li>
-                    <a href="{{ $paginator->previousPageUrl() }}" class="blogIn__pagination__link__left"rel="prev" aria-label="@lang('pagination.previous')"><i class="fas fa-chevron-left"></i></a>
-                </li>
+                <a href="{{ $paginator->previousPageUrl() }}" class="prev">&laquo;</a>
             @endif
 
             @foreach ($elements as $element)
-                @if (is_string($element))
-                    <li class="disabled" aria-disabled="true"><span>{{ $element }}</span></li>
-                @endif
-
                 @if (is_array($element))
                     @foreach ($element as $page => $url)
                         @if ($page == $paginator->currentPage())
-                            <li><a class="blogIn__pagination__link active">{{ $page }}</a></li>
-                        @else
-                            <li><a class="blogIn__pagination__link" href="{{ $url }}">{{ $page }}</a></li>
+                           <a class="active">{{ $page }}</a>
                         @endif
                     @endforeach
                 @endif
             @endforeach
 
             @if ($paginator->hasMorePages())
-                <li>
-                    <a href="{{ $paginator->nextPageUrl() }}" class="blogIn__pagination__link__left" rel="next" aria-label="@lang('pagination.next')"><i class="fas fa-chevron-right"></i></a>
-                </li>
+                <a href="{{ $paginator->nextPageUrl() }}" class="next">&raquo;</a>
             @endif
-        </ul>
-        </nav>
-    {{-- </div> --}}
+            </div>
+        </div>
 @endif
